8fd7b2c6aa47875dee283e1346d0f4fc62d19950
Vishwa thimira Vidarshana 1234 <thimira.vishwa2003@gmail.com>
2025-12-24 20:49:33 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,14 @@
 
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 
-export const firebaseConfig = {"apiKey":"AIzaSyA888y9b31GS3tF9aC2t3yT1QWy2c4AW3A","authDomain":"fir-studio-demos.firebaseapp.com","projectId":"fir-studio-demos","storageBucket":"fir-studio-demos.appspot.com","messagingSenderId":"981055254921","appId":"1:981055254921:web:4e701c6291fd2196d44a85","measurementId":"G-56VFEBEG4G"};
+export const firebaseConfig = {
+  apiKey: "AIzaSyBhXESdlpF8wCVPh7Z2LDbjXWJiDeqpdAg",
+  authDomain: "studio-2634168358-5e9d6.firebaseapp.com",
+  projectId: "studio-2634168358-5e9d6",
+  storageBucket: "studio-2634168358-5e9d6.firebasestorage.app",
+  messagingSenderId: "348605322351",
+  appId: "1:348605322351:web:b7d0f7c6163fdaa942e99a"
+};
 
 function initializeFirebase(): FirebaseApp {
     if (getApps().length) {
