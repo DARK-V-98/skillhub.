af4064a994e7c46c0813f7a040a59581317eb3af
Vishwa thimira Vidarshana 1234 <thimira.vishwa2003@gmail.com>
2025-12-24 20:43:26 +0000

    Try fixing this error: `Build Error: Export signOut doesn't exist in tar

--- src/firebase/auth.tsx
+++
@@ -1,9 +0,0 @@
-
-'use client';
-
-import { ReactNode } from "react";
-
-// This file is a placeholder. The actual auth logic is in `auth.ts` and `auth/use-user.tsx`
-export function AuthProvider({ children }: { children: ReactNode}) {
-    return <>{children}</>
-}
