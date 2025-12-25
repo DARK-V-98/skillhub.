2044fe8747d677b3c0384f369c817382c680645b
Vishwa thimira Vidarshana 1234 <thimira.vishwa2003@gmail.com>
2025-12-25 12:43:50 +0000

    Try fixing this error: `Console Error: A component is changing an uncont

--- src/components/teacher-registration/MultiStepForm.tsx
+++ src/components/teacher-registration/MultiStepForm.tsx
@@ -35,6 +35,7 @@ export default function MultiStepForm() {
         fullName: '',
         email: '',
         phone: '',
+        dateOfBirth: '',
         bio: '',
         headline: '',
         areasOfExpertise: [],
